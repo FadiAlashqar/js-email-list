ce83c3c7940e554e0d8dbd9e525d5c1e9d1d265b
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-04-03 16:58:52 +0200

    [UPD] mail generator done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 
-const mails = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
-axios.get(mails).then((resp) => {
+for (let i = 0; i < 10; i++) {
+    
+    const emailContainer = document.getElementById('email-container');
+    
+    const mails = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
-let randomMails = resp.data.response;
-console.log(randomMails)
-
-});
+    axios.get(mails).then((resp) => {
+    
+        let randomMails = resp.data.response;
+        let listElement = document.createElement('li');
+        listElement.innerText = randomMails;
+        emailContainer.appendChild(listElement);
+    
+    });
+};
